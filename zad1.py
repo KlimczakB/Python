7f61cfde00d01e3318a054389aa54a82c4102791
KlimczakB <101150792+KlimczakB@users.noreply.github.com>
2022-07-06 15:59:04 +0200

    Add files via upload

---
+++ zad1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+#Plot functions sin(x), cos(x), and exp(-x) in a range [0,10]. 
+#Colors are red, green, and blue, respectively. 
+#Lines are solid, dashed, and dotted, respectively. Add a legend.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,10,200)
+y1 = np.sin(x)
+y2 = np.cos(x)
+y3 = np.exp(-x)
+
+plt.plot(x, y1, color='red', linestyle='solid', label='sin(x)')
+plt.plot(x, y2, color='green', linestyle='dashed', label='cos(x)')
+plt.plot(x, y3, color='blue', linestyle='dotted', label='exp(-x)')
+plt.title('Exercise 8.1')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend() 
+plt.show()
